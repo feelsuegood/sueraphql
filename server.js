b9c5ceba3ca48f2c5c0f589f58831752c663bfd9
feelsuegood <suaj2021@gmail.com>
2025-04-16 20:00:14 +1000

    4.6 query resolvers

--- server.js
+++ server.js
@@ -3,6 +3,11 @@
 //  * "type": "module" => ES Modules
 import { ApolloServer, gql } from "apollo-server";
 
+const tweets = [
+  { id: "1", text: "first one" },
+  { id: "2", text: "second one" },
+];
+
 const typeDefs = gql`
   type User {
     id: ID!
@@ -12,8 +17,8 @@ const typeDefs = gql`
   }
   type Tweet {
     id: ID!
-    title: String!
-    author: User!
+    text: String!
+    author: User
   }
   type Query {
     allTweets: [Tweet!]!
@@ -25,7 +30,18 @@ const typeDefs = gql`
   }
 `;
 
-const server = new ApolloServer({ typeDefs });
+const resolvers = {
+  Query: {
+    allTweets() {
+      return tweets;
+    },
+    tweet(root, { id }) {
+      return tweets.find((tweet) => tweet.id === id);
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
   console.log(`Running on ${url}`);
